3ab38a84af1d191fb64f4184207c517d477ec63e
Paul <p.steppacher@gmx.de>
2020-05-06 11:04:16 +0200

    Refactored error message function

--- close-tabs.js
+++ close-tabs.js
@@ -7,42 +7,22 @@ function onUpdated(tab){
     setTimeout(()=>browser.tabs.remove(tab.id), 1000);
 }
 
-function onError(error) {
-    console.log(`Error from Tab removing: ${error}`);
-}
-function onErrorUpdate(error) {
-console.log(`Error from Tab redirecting: ${error}`);
+function onError(message){
+    return (error) => console.log(`Error from ${message}: ${error}`);
 }
 
 async function handleCreated(tab){
 
     console.log("Tab created with ID: " + tab.id+ " and index " + tab.index);
-/*     if(tab.index > 5 && !tab.id !== browser.tabs.TAB_ID_NONE ){
-        browser.tabs.remove(tab.id).then(onRemoved, onError);
-    }
-    if(tab.index == 5 && !tab.id !== browser.tabs.TAB_ID_NONE ){
-        browser.tabs.update(
-            tab.id,
-            {url: "substitute-page/page.html"})
-            .then(onUpdated, onErrorUpdate);
-    } */
-/*     browser.tabs.query({currentWindow: true}).then((tabs) =>{
-        let length = tabs.length;
-        if(length>5){
-            
-            let rem = browser.tabs.remove(tabId.id);
-            rem.then(onRemoved, onError)
-        }
-    }) */
     let tabs = await browser.tabs.query({currentWindow: true})
     if(tabs.length > 5 && !tab.id !== browser.tabs.TAB_ID_NONE ){
-        browser.tabs.remove(tab.id).then(onRemoved, onError);
+        browser.tabs.remove(tab.id).then(onRemoved, onError('removing'));
     }
     if(tabs.length == 5 && !tab.id !== browser.tabs.TAB_ID_NONE ){
         browser.tabs.update(
             tab.id,
             {url: "substitute-page/page.html"})
-            .then(onUpdated, onErrorUpdate);
+            .then(onUpdated, onError('redirecting'));
     }
 };
 
